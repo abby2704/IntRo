212bcd67daf8bcf48eb11ab1d9fade067ec546b2
Nicolas-Schmidt <nicoschlab@gmail.com>
2018-11-27 23:28:59 -0300

    type

--- Presentaciones/codigo/coerciones_implicitas.R
+++ Presentaciones/codigo/coerciones_implicitas.R
@@ -8,5 +8,5 @@ tabla <- matrix(0, 4, 4, dimnames = list(modes,modes))
 for(i in 1:4) for(j in 1:4) {
         tabla[i, j] <- typeof(c(vector(tipos[i]), vector(tipos[j])))
 }
-noquote(modarr)
+noquote(tabla)
 
