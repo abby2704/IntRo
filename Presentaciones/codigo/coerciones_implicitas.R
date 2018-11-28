84948e709b2cefac3005caa030014973bd67bc7c
Nicolas-Schmidt <nicoschlab@gmail.com>
2018-11-27 23:31:02 -0300

    typeof

--- Presentaciones/codigo/coerciones_implicitas.R
+++ Presentaciones/codigo/coerciones_implicitas.R
@@ -4,7 +4,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 tipos <- c("logical", "integer", "double", "character")
-tabla <- matrix(0, 4, 4, dimnames = list(modes,modes))
+tabla <- matrix(0, 4, 4, dimnames = list(tipos, tipos))
 for(i in 1:4) for(j in 1:4) {
         tabla[i, j] <- typeof(c(vector(tipos[i]), vector(tipos[j])))
 }
