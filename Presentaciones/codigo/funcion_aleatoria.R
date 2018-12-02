f0232f8934ac964f731f20ac09a9ff603b54b63a
Nicolas-Schmidt <nicoschlab@gmail.com>
2018-12-02 12:37:57 -0300

    dropwhich

--- Presentaciones/codigo/funcion_aleatoria.R
+++ Presentaciones/codigo/funcion_aleatoria.R
@@ -5,7 +5,7 @@
 
 funcion_diaria <- function(paquete = "base"){
         r <- search()
-        l <- ls(r[which(r == paste0("package:", paquete))])
+        l <- ls(r[r == paste0("package:", paquete)])
         f <- sample(l, 1)
         cat("The function of the day is:", f)
         help(f)
@@ -13,3 +13,7 @@ funcion_diaria <- function(paquete = "base"){
 
 funcion_diaria()
 funcion_diaria("stats")
+
+
+
+
